802e70b6fbd6c4aaf86e68383c5c42b49904b7a0
AgarkarPranav <ppdagarkar@gmail.com>
2022-01-25 20:05:24 +0530

    first commit123456

--- uniq/settings.py
+++ uniq/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-tjtq#gx@zz79ndla4-0!%rv_5ix9^evkpznrwq2^rsd7x%#$-#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*','192.168.0.106','prcalculator.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
